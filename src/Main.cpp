312621ea8ef0e073d2210453385189cbeffddf5c
eric <a@a>
2019-12-02 21:04:50 +0200

    Main file delieverd

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include "../include/Session.h"
 using namespace std;
-Session bar(char*  filepath);
 
 int main(int argc, char** argv){
     if(argc!=2)
@@ -10,15 +9,7 @@ int main(int argc, char** argv){
         return 0;
     }
     Session s(argv[1]);
-    Session s2 (bar(argv[1]));
-    s2.start();
-//    Session *s1(new Session(argv[1]));
     s.start();
     return 0;
 }
 
-Session bar(char* filepath) {
-    Session s(filepath);
-    s.start();
-    return s;
-}
